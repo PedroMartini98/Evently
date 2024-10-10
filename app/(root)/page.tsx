e54eec9f5789ef61f1001603a009db8a8f6f2cf8
Pedro H. Martini <Pedroh.martini@yahoo.com.br>
2024-10-10 18:07:47 -0300

    i'm desperate

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
         className="wrapper my-8 flex flex-col gap-8 md:gap-12"
       >
         <h2 className="h2-bold">
-          Trusted by <br /> Thounsands of Events
+          Trusted by <br /> Thounsands Of Events
         </h2>
         <div className="flex w-full flex-col gap-5 md:flex-row">
           Search Category filter
