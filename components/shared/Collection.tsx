ecdfbea3807c7ccd4795702b11be60d2e3dcf262
Pedro H. Martini <Pedroh.martini@yahoo.com.br>
2024-11-05 19:11:51 -0300

    Deploy

--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -1,6 +1,7 @@
 import { IEvents } from "@/lib/mongodb/database/models/event.model";
 import React from "react";
 import Card from "./Card";
+import Pagination from "./Pagination";
 
 type CollectionProps = {
   data: IEvents[];
@@ -44,6 +45,14 @@ const Collection = ({
               );
             })}
           </ul>
+
+          {totalPages > 1 && (
+            <Pagination
+              urlParamName={urlParamName}
+              page={page}
+              totalPages={totalPages}
+            />
+          )}
         </div>
       ) : (
         <div
